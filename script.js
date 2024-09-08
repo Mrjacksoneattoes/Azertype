4b34334b3c7fd2237c3c585221867172c554fe99
B10 <bedis.moat24@hotmail.com>
2024-09-08 14:08:34 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let body = document.querySelector("body");
+let header = document.querySelector("header")
 let main = document.querySelector("main");
 let footer = document.querySelector("footer");
 let radios = document.querySelectorAll('.big input');
@@ -6,6 +7,8 @@ let bouton = document.querySelector(".btn");
 let response = document.getElementById("reponse");
 let affichage = document.getElementById("phrase");
 let scoreAffiche = document.getElementById("score");
+let partager = document.getElementById("partager");
+let card = document.querySelector(".form_container");
 const mots = [
     "Pomme",
     "Banane",
@@ -106,20 +109,31 @@ const mots = [
 
   let score = 0;
   let randomWord = null;
-  let randomPhrase = null
+  let randomPhrase = null;
+
+  document.addEventListener('click', (e) => {
+
+    if (!card.contains(e.target)) {
+      card.style.display = "none";
+      main.classList.remove("opacite");
+      footer.classList.remove("opacite");
+      header.classList.remove("opacite");
+    }
+  
+  });
 
   function choix(){
     for (let i =0; i < radios.length; i++){
         let choixradios = radios[i];
         choixradios.addEventListener("click", (e)=>{
             if (e.target.id === "Mots"){
-                affichageMot()
-                score = 0
+                affichageMot();
+                score = 0;
                 scoreAffiche.textContent = `Votre score: ${score}/${mots.length}`
             }else if (e.target.id === "Phrases"){
-                affichagePhrase()
-                score = 0
-                scoreAffiche.textContent = `Votre score: ${score}/${phrases.length}`
+                affichagePhrase();
+                score = 0;
+                scoreAffiche.textContent = `Votre score: ${score}/${phrases.length}`;
 
             }
             
@@ -129,14 +143,14 @@ const mots = [
     }
 
   }
+
 function affichageMot(){
     for (let a = 0; a < mots.length; a++){
         let randomIndex = Math.floor(Math.random()*mots.length);
-        randomWord = mots[randomIndex]
-        affichage.textContent = randomWord
-        console.log("le mot choisi est "+randomWord)
-       
-        return randomWord
+        randomWord = mots[randomIndex];
+        affichage.textContent = randomWord;
+        console.log("le mot choisi est "+randomWord);
+        return randomWord;
 
     }
 }
@@ -146,33 +160,73 @@ function affichagePhrase(){
         let randomIndex = Math.floor(Math.random()*phrases.length);
         randomPhrase = phrases[randomIndex];
         affichage.textContent = randomPhrase;
-        console.log("la phrase choisie est "+randomPhrase)
-    return randomPhrase        
+        console.log("la phrase choisie est "+randomPhrase);
+    return randomPhrase;        
     }
 }
   
+function affichageForm(){
+    partager.addEventListener("click", (e)=>{
+        e.stopPropagation();
+        card.style.display = "flex";
+        card.classList.add("dansLeBody");
+        card.classList.add("cardOpacity");
+        body.appendChild(card);
+        main.classList.add("opacite");
+        footer.classList.add("opacite");
+        header.classList.add("opacite");
+       
 
-  function lancerScript(){
-    choix()
-    
-    bouton.addEventListener("click", (e)=>{
-        if (response.value === randomWord){
-            score++
-            affichageMot()
-            console.log(score)
-            scoreAffiche.textContent = `Votre score: ${score}/${mots.length}`
-            response.value = ""
-        }
-        else if (response.value === randomPhrase){
-            score++
-            affichagePhrase()
-            console.log(score)
-            scoreAffiche.textContent = `Votre score: ${score}/${phrases.length}`
-            response.value = ""
-        }
     })
     
-  }
- 
+}
 
+function verificationEmail(emailVer){
+    card.addEventListener("submit", (e)=>{
+    let email = document.getElementById("email_field");
+    let regex = new RegExp("^[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9_.-]")
+    if(regex.test(email.value) === false){
+        e.preventDefault()
+        console.log('y a r gros');
+        email.classList.add('borderError')
+    }else if(regex.test(email.value) === true){
+        email.classList.remove(borderError)
+    }
+        
+        
+    })
+}
+
+  function lancerScript(){
+    choix();
+    bouton.addEventListener("click", (e)=>{
+        if (response.value === randomWord){
+            score++;
+            affichageMot();
+            console.log(score);
+            scoreAffiche.textContent = `Votre score: ${score}/${mots.length}`;
+            response.value = "";
+        }
+        else if (response.value === randomPhrase){
+            score++;
+            affichagePhrase();
+            console.log(score);
+            scoreAffiche.textContent = `Votre score: ${score}/${phrases.length}`;
+            response.value = "";
+        }else if (response.value !== randomWord){
+            score = 0;
+            scoreAffiche.textContent = `Votre score: ${score}/${mots.length}`;
+            response.value = "";
+
+        }else if(response.value !== randomPhrase){
+            score = 0;
+            scoreAffiche.textContent = `Votre score: ${score}/${phrases.length}`;
+            response.value = "";
+        }
+        
+       
+    })
+    affichageForm()
+     verificationEmail()
+  }
   lancerScript()
